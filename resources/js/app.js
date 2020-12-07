6deca39d14c0257b0068db19eee89757ef2dad9b
BoCoder <agarrido84@gmail.com>
2020-12-06 21:58:48 -0500

    nav bar toggle finished

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 let controller;
 const burger = document.querySelector('.burger');
+const projectNames = document.querySelectorAll('.project')
 
 // functions
 function introAnimation (){
@@ -14,11 +15,36 @@ function introAnimation (){
 }
 
 function navToggle(e){
-    gsap.to('.line1', 0.5, {rotate: '45', y: 5})
-    gsap.to('.line2', 0.5, {rotate: '-45', y: -5})
-    gsap.to('.nav-bar', 1, {clipPath: 'circle(3000px at 100% -10%)'})
+    const navHeader = document.querySelector('.nav-header')
+    const burger = document.querySelector('.burger')
+    const navList = document.querySelectorAll('.nav-index li a')
+    if(e.target.classList.contains('active')){
+     gsap.to('.line1', 0.5, {rotate: '0', y: 0})
+     gsap.to('.line2', 0.5, {rotate: '0', y: 0})
+     gsap.to('.nav-bar', 1, {clipPath: 'circle(30px at 100% -10%)'})
+     e.target.classList.remove('active')
+     gsap.to(navHeader, 1, { ease: "power1.out", background: "#b9bfc4"})
+     gsap.to(navList, 2, {color: "#374054", pointerEvents: 'auto'})
+     document.body.classList.remove('hide')
+    } else {
+     gsap.to('.line1', 0.5, {rotate: '45', y: 5})
+     gsap.to('.line2', 0.5, {rotate: '-45', y: -5})
+     gsap.to('.nav-bar', 1, {clipPath: 'circle(3000px at 100% -10%)'})
+     e.target.classList.add('active')
+     gsap.to(navHeader, 0.2, {background: "white"})
+     gsap.to(navList, 0.2, {color: "white", pointerEvents: 'none'})
+     document.body.classList.add('hide')
+    //  navHeader.style.background = 'white'
+    }
 }
 
+function projToggle(e){
+    console.log(e)
+}
+
+projectNames.addEventListener('click', (e)=>{
+    projToggle(e);
+});
 // burger animation 
 burger.addEventListener('click', navToggle);
 introAnimation()
